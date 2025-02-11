420c9764467dceee2b8b57df395a80a458699cfb
alejandro <alejandroriveros100@gmail.com>
2025-02-11 09:05:02 -0500

    fix

--- src/main/java/com/artesanias/gateway/config/GatewayConfig.java
+++ src/main/java/com/artesanias/gateway/config/GatewayConfig.java
@@ -7,10 +7,8 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
-
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "API Gateway",version = "1.0", description = "API Gateway"))
+@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "API Gateway"))
 public class GatewayConfig {
 
     private final AuthenticationFilter authenticationFilter;
@@ -18,8 +16,9 @@ public class GatewayConfig {
     public GatewayConfig(AuthenticationFilter authenticationFilter) {
         this.authenticationFilter = authenticationFilter;
     }
+
     @Bean
-    public RouteLocator routes(RouteLocatorBuilder builder, RouteLocator routes){
+    public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("auth-service", r -> r.path("/auth/**")
                         .filters(f -> f.filter(authenticationFilter))
@@ -27,6 +26,15 @@ public class GatewayConfig {
                 .route("user-service", r -> r.path("/user/**")
                         .filters(f -> f.filter(authenticationFilter))
                         .uri("lb://user-service"))
+                .route("commerce-service", r -> r.path("/commerce/**")
+                        .filters(f -> f.filter(authenticationFilter))
+                        .uri("lb://commerce-service"))
+                .route("payment-service", r -> r.path("/payment/**")
+                .filters(f -> f.filter(authenticationFilter))
+                        .uri("lb://payment-service"))
+                .route("management-service", r -> r.path("/management/**")
+                .filters(f -> f.filter(authenticationFilter))
+                .uri("lb://management-service"))
                 .build();
 
     }
